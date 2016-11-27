1bd15c87312d31fa7078db3bb346f4275997dea8
xulingming <janeysesions@gmail.com>
2016-11-27 14:08:49 +0800

    [ADD]add project util

--- server/api/util/index.js
+++ server/api/util/index.js
@@ -1,6 +1,9 @@
 'use strict';
 
 import mongoose from 'mongoose';
+import moment from 'moment';
+import _ from 'lodash';
+var _object = require('lodash/fp/object');
 
 var Util = {};
 
@@ -16,9 +19,9 @@ Util.convertId2ObjectID = function (entity) {
         if (entity[attr]
           && attr.toString().length >= 3
           && attr.toString().endsWith('_id')) {
-          console.log('<1>%s<2>%s<3>%s', attr, attr.toString(), attr.indexOf('_id'));
-          entity[attr] = new mongoose.Types.ObjectId;//(entity[attr]);
-          console.log('convert string id 2 objectID: %s --> %s', entity[attr], entity[attr]);
+          // console.log('<1>%s<2>%s<3>%s', attr, attr.toString(), attr.indexOf('_id'));
+          entity[attr] = new mongoose.Types.ObjectId(entity[attr]);
+          // console.log('convert string id 2 objectID: %s --> %s', entity[attr], entity[attr]);
         }
       }
     }
@@ -26,6 +29,29 @@ Util.convertId2ObjectID = function (entity) {
     console.log('id convert happened wrong:' + e);
   } finally {
     return entity;
+  }
+}
+
+Util.convertDateWithFormat = function (entity) {
+  var nwEntity = entity;
+  console.log('< 1 >-----------------------------------' + JSON.stringify(nwEntity));
+  try {
+    var format = 'YYYY/MM/DD HH:mm:ss';
+    for (var attr in entity) {
+      nwEntity[attr] = entity[attr];
+      if (_.isDate(entity[attr])) {
+        // var d = new Date(entity[attr]);
+        // var formatTimestamp = JSON.stringify(moment(d).format(format));
+        nwEntity.what = "formatTimestamp";
+        break;
+      }
+    }
+    nwEntity.remark = 'Hi whats wrong?';
+  } catch (e) {
+    console.log('convert date object happened wrong:' + e.toString());
+  } finally {
+    console.log('< 2 >-----------------------------------' + JSON.stringify(nwEntity));
+    return nwEntity;
   }
 }
 module.exports = Util;
