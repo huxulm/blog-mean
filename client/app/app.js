3b2b0e276037542abcf028a9b26f5860cf81bcd1
jackdon <janeysesions@gmail.com>
2017-04-13 16:31:37 +0800

    [MDF] ngMessage

--- client/app/app.js
+++ client/app/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('snoopyApp', ['snoopyApp.auth', 'snoopyApp.admin', 'snoopyApp.home', 'snoopyApp.blog', 'snoopyApp.about', 'snoopyApp.blog.private.gallery',
-    'snoopyApp.constants', 'ngCookies', 'ngResource', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'validation.match', 'ngMaterial', 'oitozero.ngSweetAlert'
+    'snoopyApp.constants', 'ngCookies', 'ngResource', 'ngMessages', 'ngSanitize', 'ui.router', 'ui.bootstrap', 'validation.match', 'ngMaterial', 'oitozero.ngSweetAlert'
   ])
   .config(function($urlRouterProvider, $locationProvider) {
     $urlRouterProvider.otherwise('/');
