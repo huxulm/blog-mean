222285fefbea454f95fc3d37368e398b00e61036
jackdon <janeysesions@gmail.com>
2017-04-13 16:30:07 +0800

    [MDF] clean

--- client/app/private/gallery/gallery.directive.js
+++ client/app/private/gallery/gallery.directive.js
@@ -8,32 +8,19 @@
       priority: 0,
       transclude: false,
       restrict: 'EA',
-      // templateNamespace: 'html',
       scope: false,
       controller: 'GalleryCtrl',
       controllerAs: 'galCtrl',
       bindToController: true,
-      // require: 'siblingDirectiveName', // or // ['^parentDirectiveName', '?optionalDirectiveName', '?^optionalParent'],
       multiElement: false,
       compile: function compile(tElement, tAttrs, transclude) {
         return {
           pre: function preLink(scope, iElement, iAttrs, galCtrl) {
             console.log('Attrs:', iElement, iAttrs, galCtrl);
+            galCtrl.loadAlbums();
           },
-          post: function postLink(scope, iElement, iAttrs, controller) {
-            console.log('Attrs:', iElement, iAttrs, controller);
-            lightGallery(document.getElementById('gallery_id'),{
-              width: '700px',
-              height: '470px',
-              mode: 'lg-lollipop',
-              addClass: 'fixed-size',
-              counter: false,
-              download: false,
-              startClass: '',
-              enableSwipe: false,
-              enableDrag: false,
-              speed: 500
-            });
+          post: function postLink(scope, iElement, iAttrs, galCtrl) {
+            console.log('Attrs:', iElement, iAttrs, galCtrl);
           }
         };
       }
